42eea27cd871b16dd9cca6d8c73cc56689060133
Alexander Le <alexandervanleprivat@gmail.com>
2025-02-24 20:25:28 +0100

    wip

--- app/puppeteer/browser.ts
+++ app/puppeteer/browser.ts
@@ -1,21 +1,16 @@
+"use server";
+
 import chromium from "@sparticuz/chromium";
 import puppeteerCore from "puppeteer-core";
-import puppeteer from "puppeteer";
 
 export async function getBrowser() {
-  const isDev = process.env.NODE_ENV === "development";
-
-  if (isDev) {
-    return puppeteer.launch({
-      headless: true,
-    });
-  }
-
   // Production environment (e.g., AWS Lambda)
   return puppeteerCore.launch({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath(),
+    executablePath: await chromium.executablePath(
+      "https://github.com/Sparticuz/chromium/releases/download/v132.0.0/chromium-v132.0.0-pack.tar"
+    ),
     headless: true,
   });
 }
