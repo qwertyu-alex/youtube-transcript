a5590debb414a7d11c70c3d5a4a1909e578cbea6
Alexander Le <alexandervanleprivat@gmail.com>
2025-02-28 22:33:38 +0100

    wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,19 @@ export const metadata: Metadata = {
   icons: {
     icon: "/favicon.png",
   },
+  openGraph: {
+    images: [
+      {
+        url: "https://gxbsjhzsyzmqllluvkof.supabase.co/storage/v1/object/public/youtube-transcript-gen/yt-open-graph.png",
+      },
+    ],
+    title: "Free YouTube Transcript Generator",
+    description:
+      "Fastest way to get transcripts from Youtube videos - Open source on GitHub",
+    siteName: "YouTube Transcript Generator",
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
